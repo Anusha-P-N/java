e475af957fddf66bf9cb030dd7616d8afe74f754
Vidya P N <Vidya P N@192.168.1.56>
2025-05-18 15:55:14 +0530

    Second commit

---
+++ java_training/src/com/atriaday4/MainClass.java
@@ -0,0 +1,15 @@
+package com.atriaday4;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Citizen c = new Citizen("Bangalore", "Rahul", "1234-5678-9012", 9876543210L);
+        System.out.println("Name: " + c.getName());
+        System.out.println("Address: " + c.getAddress());
+        System.out.println("Aadhar No: " + c.getAadharNo());
+        System.out.println("Phone No: " + c.getPhNo());
+
+	}
+
+}
